a673d54fb5ae74b4b293902328f595b79c0c8ed6
Joseph Kain <joekain@gmail.com>
2018-06-10 14:23:01 -0700

    Parse Literals

--- src/projectlib/src/parser.h
+++ src/projectlib/src/parser.h
@@ -1,10 +1,39 @@
 #include <string.h>
+#include <variant>
 
 namespace regex_compiler {
+
+// Literals hold a string of characters
+struct Literal {
+  char pattern;
+};
+
+using Term = std::variant<Literal>;
 
 class Parser {
  public:
   Parser(std::string s) {
+      for (auto c : s) {
+        Term t{Literal{c}};
+        terms.push_back(t);
+      }
   }
+
+  using iterator = std::vector<Term>::iterator;
+
+  size_t size() {
+    return terms.size();
+  }
+
+  iterator begin() noexcept {
+    return terms.begin();
+  }
+
+  iterator end() noexcept {
+    return terms.end();
+  }
+
+ private:
+  std::vector<Term> terms;
 };
 }  // namespace regex_compiler
--- src/projectlib/test/test_parser.cc
+++ src/projectlib/test/test_parser.cc
@@ -1,6 +1,5 @@
 #include "catch.hpp"
 
-#include <string>
 #include "../src/parser.h"
 
 using namespace std;
@@ -8,4 +7,30 @@ using namespace regex_compiler;
 
 TEST_CASE("The empty string can be parsed", "[Parser]") {
     auto p = Parser("");
+}
+
+TEST_CASE("A Parser behaves like a container") {
+    auto p = Parser("");
+
+    size_t s = p.size();
+    Parser::iterator b = p.begin();
+    Parser::iterator e = p.end();
+
+    (void)s;
+}
+
+TEST_CASE("A single letter pattern can be parsed", "[Parser]") {
+  auto p = Parser("a");
+
+  CHECK(p.size() == 1);
+  CHECK_NOTHROW(get<Literal>(*begin(p)));
+  CHECK(get<Literal>(*begin(p)).pattern == 'a');
+}
+
+TEST_CASE("A two letter pattern can be parsed", "[Parser]") {
+  auto p = Parser("ab");
+
+  CHECK(p.size() == 2);
+  CHECK_NOTHROW(get<Literal>(*begin(p)));
+  CHECK(get<Literal>(*begin(p)).pattern == 'a');
 }
