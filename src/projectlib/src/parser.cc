69de8a11e9aecca94fdd6945c69265f510b19a48
Joseph Kain <joekain@gmail.com>
2018-06-10 18:23:48 -0700

    Move most parser code to parser.cc
    Move constructor to parser.cc
    Move most of the private member functions to parser.cc as static
    non-member functions.

--- src/projectlib/src/parser.cc
+++ src/projectlib/src/parser.cc
@@ -1,8 +1,65 @@
+#include "parser.h"
+
 namespace regex_compiler {
 
-    class Parser {
+static Term parse(std::string::iterator &it, std::string::iterator end);
 
-
-    };
-
+static Term parse_literal(char c) {
+  return Term{Literal{c}};
 }
+
+static Terms parse_until(std::string::iterator &it, std::string::iterator end, char end_char) {
+  Terms local_terms;
+  for (; it != end && *it != end_char; it++) {
+    Term t = parse(it, end);
+    local_terms.push_back(t);
+  }
+
+  return local_terms;
+}
+
+static Term parse_group(std::string::iterator &it, std::string::iterator end) {
+  Terms group_terms = parse_until(it, end, ')');
+  if (*it != ')') throw std::invalid_argument("Unmatched \'(\'");
+
+  return Term{Group{group_terms}};
+}
+
+static void parse_kleene_lookahead(std::string::iterator &it, std::string::iterator end, Term &t) {
+  if (it != end && *(it + 1) == '*') {
+    it += 1;  // skip over *
+    t = Term{Kleene{Terms{t}}};
+  }
+}
+
+static void parse_alternative_lookahead(std::string::iterator &it, std::string::iterator end,
+                                        Term &t) {
+  if (it != end && *(it + 1) == '|') {
+    it += 2;  // skip over |
+    Term t2 = parse(it, end);
+    t = Term{Alternative{Terms{t, t2}}};
+  }
+}
+
+Term parse(std::string::iterator &it, std::string::iterator end) {
+  auto c = *it;
+  Term t;
+  if (c == '(') {
+    it++;
+    t = parse_group(it, end);
+  } else {
+    t = parse_literal(c);
+  }
+
+  parse_kleene_lookahead(it, end, t);
+  parse_alternative_lookahead(it, end, t);
+
+  return t;
+}
+
+Parser::Parser(std::string s) {
+  auto it = s.begin();
+  terms = parse_until(it, s.end(), 0);
+}
+
+}  // namespace regex_compiler
--- src/projectlib/src/parser.h
+++ src/projectlib/src/parser.h
@@ -32,63 +32,8 @@ struct Kleene {
 };
 
 class Parser {
-  Term parse_literal(char c) {
-    return Term{Literal{c}};
-  }
-
-  Terms parse_until(std::string::iterator &it, std::string::iterator end, char end_char) {
-    Terms local_terms;
-    for (; it != end && *it != end_char; it++) {
-      Term t = parse(it, end);
-      local_terms.push_back(t);
-    }
-
-    return local_terms;
-  }
-
-  Term parse_group(std::string::iterator &it, std::string::iterator end) {
-    Terms group_terms = parse_until(it, end, ')');
-    if (*it != ')') throw std::invalid_argument("Unmatched \'(\'");
-
-    return Term{Group{group_terms}};
-  }
-
-  void parse_kleene_lookahead(std::string::iterator &it, std::string::iterator end, Term &t) {
-    if (it != end && *(it + 1) == '*') {
-      it += 1;  // skip over *
-      t = Term{Kleene{Terms{t}}};
-    }
-  }
-
-  void parse_alternative_lookahead(std::string::iterator &it, std::string::iterator end, Term &t) {
-    if (it != end && *(it + 1) == '|') {
-      it += 2;  // skip over |
-      Term t2 = parse(it, end);
-      t = Term{Alternative{Terms{t, t2}}};
-    }
-  }
-
-  Term parse(std::string::iterator &it, std::string::iterator end) {
-    auto c = *it;
-    Term t;
-    if (c == '(') {
-      it++;
-      t = parse_group(it, end);
-    } else {
-      t = parse_literal(c);
-    }
-
-    parse_kleene_lookahead(it, end, t);
-    parse_alternative_lookahead(it, end, t);
-
-    return t;
-  }
-
  public:
-  Parser(std::string s) {
-    auto it = s.begin();
-    terms = parse_until(it, s.end(), 0);
-  }
+  Parser(std::string s);
 
   using iterator = Terms::iterator;
 
